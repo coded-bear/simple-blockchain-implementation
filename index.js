25997995b881cc67967df99599ab6bf6ac4536ec
Łukasz Warchoł <lukasz.warchol14@gmail.com>
2020-01-12 21:50:11 +0100

    fixed addBlock function, added isChanValid function and added test simulation

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Simple blockchain implementation");
+const Blockchain = require("./lib/blockchain");
+
+const chain = new Blockchain();
+
+chain.addBlock({ amount: 100 });
+chain.addBlock({ amount: 50 });
+chain.addBlock({ amount: 200 });
+
+console.log(chain);
+console.log("Is chain valid: ", chain.isChainValid());
